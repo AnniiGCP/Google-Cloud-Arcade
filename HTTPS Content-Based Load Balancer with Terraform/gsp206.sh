60f6abcab21a59b2d3f13698c82b8ac4b321eef8
Aniket Yadav <186996280+AnniiGCP@users.noreply.github.com>
2025-06-14 11:41:13 +0530

    Update gsp206.sh

--- HTTPS Content-Based Load Balancer with Terraform/gsp206.sh
+++ HTTPS Content-Based Load Balancer with Terraform/gsp206.sh
@@ -1,39 +1,43 @@
-#!/bin/bash
-# Define color variables
 
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
+echo ""
+echo ""
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
+read -p "Enter Group1_Region :- " group1_region
+read -p "Enter Group2_Region :- " group2_region
+read -p "Enter Group3_Region :- " group3_region
 
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
 
-echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
-
-gcloud auth list
-
-git clone https://github.com/GoogleCloudPlatform/terraform-google-lb-http.git
+git clone https://github.com/terraform-google-modules/terraform-google-lb-http.git
 
 cd ~/terraform-google-lb-http/examples/multi-backend-multi-mig-bucket-https-lb
 
 rm -rf main.tf
 
-wget https://raw.githubusercontent.com/AnniiGCP/Google-Cloud-Arcade/main/HTTPS%20Content-Based%20Load%20Balancer%20with%20Terraform/main.tf
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/HTTPS%20Content-Based%20Load%20Balancer%20with%20Terraform/main.tf
+
+
+cat > variables.tf <<EOF_END
+variable "group1_region" {
+  default = "$group1_region"
+}
+
+variable "group2_region" {
+  default = "$group2_region"
+}
+
+variable "group3_region" {
+  default = "$group3_region"
+}
+
+variable "network_name" {
+  default = "ml-bk-ml-mig-bkt-s-lb"
+}
+
+variable "project" {
+  type = string
+}
+EOF_END
+
 
 terraform init 
 
@@ -41,6 +45,6 @@ echo $DEVSHELL_PROJECT_ID | terraform plan
 
 echo $DEVSHELL_PROJECT_ID | terraform apply -auto-approve
 
-echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
-#-----------------------------------------------------end----------------------------------------------------------#
+EXTERNAL_IP=$(terraform output | grep load-balancer-ip | cut -d = -f2 | xargs echo -n)
+echo http://${EXTERNAL_IP}
